b97755c352d10aa93ab152b90216a95f60a7ad3f
Stealth-J <nosayamejeffrey@gmail.com>
2026-08-11 04:16:51 +0100

    removed gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-import faulthandler
-import sys
-
-def worker_int(worker):
-    worker.log("=== WORKER INTERRUPTED - DUMPING STACK ===")
-    faulthandler.dump_traceback(file = sys.stderr, all_threads=True)
